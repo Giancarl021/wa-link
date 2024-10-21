49abbca01477105d0a9a0dbed77356127a344f85
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2024-10-21 11:13:23 -0300

    feat: Phone number from query params

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,32 @@ import 'bulma/css/bulma.min.css';
 import './scss/main.scss';
 import Inputmask from 'inputmask';
 
+function parseNumber(input: string): number | null {
+    const number = input.replace(/D/g, '');
+    if (number.length < 10) return null;
+
+    return Number(number);
+}
+
 async function main() {
-  const $input = document.querySelector('input') as HTMLInputElement;
-  const $btn = document.querySelector('button') as HTMLButtonElement;
+    const $input = document.querySelector('input') as HTMLInputElement;
+    const $btn = document.querySelector('button') as HTMLButtonElement;
 
-  $btn.addEventListener('click', () => {
-    const number = $input.value.replace(/\D/g, '');
-    if (number.length < 10) return;
-    window.open(`https://wa.me/+55${number}`, '_blank');
-  })
+    const params = new URLSearchParams(location.search);
 
-  Inputmask({ regex: String.raw`\(\d{2}\) 9?\d{4}-\d{4}` }).mask($input);  
+    const phone =
+        (params.has('phone') && parseNumber(params.get('phone')!)) || null;
+
+    if (phone) $input.value = String(phone);
+
+    $btn.addEventListener('click', () => {
+        const number = parseNumber($input.value);
+        if (!number) return;
+
+        window.open(`https://wa.me/+55${number}`, '_blank');
+    });
+
+    Inputmask({ regex: String.raw`\(\d{2}\) 9?\d{4}-\d{4}` }).mask($input);
 }
 
 document.addEventListener('DOMContentLoaded', main);
